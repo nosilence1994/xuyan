4c06b8db315d3f3f05535d5802addb1426d56c1d
nosilence <nosilence1994@hotmail.com>
2019-06-19 22:42:49 +0800

    更新index；更新join us；更新paper

--- js/google-track.js
+++ js/google-track.js
@@ -10,5 +10,5 @@
     m.parentNode.insertBefore(a, m)
   })(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
 
-  ga('create', 'UA-142404071-1', 'auto');
+  ga('create', 'UA-142423443-1', 'auto');
   ga('send', 'pageview');
